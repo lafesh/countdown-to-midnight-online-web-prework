8407f7be0ef767b2536f520feda6f6e68054c642
Gina Fedder <ginafedder@yahoo.com>
2018-09-08 05:15:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,9 +10,9 @@ end
 
 def countdown_with_sleep(number)
    while number > 0 
-    number -= 1
-    puts "#{number} SECOND(S)!"
     
+    puts "#{number} SECOND(S)!"
+    number -= 1
     sleep(1)
   end
 end
